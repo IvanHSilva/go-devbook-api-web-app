dd1f56af73f0665edda4f0f830d89d917b9913d6
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-06-08 15:37:17 -0300

    Insert User

--- src/repositories/user-repository.go
+++ src/repositories/user-repository.go
@@ -3,6 +3,9 @@ package repositories
 import (
 	"api/src/models"
 	"database/sql"
+	"fmt"
+	"log"
+	"time"
 )
 
 type users struct {
@@ -13,6 +16,33 @@ func NewUserRepository(db *sql.DB) *users {
 	return &users{db}
 }
 
-func (repo users) Insert(user models.User) (uint64, error) {
-	return 0, nil
+func (repository users) Insert(user models.User) (uint64, error) {
+	//
+	//today := time.Now()
+	//regDate := today.Format("01/02/2006")
+	//fmt.Println(today.Format("02/01/2006"))
+
+	statement, err := repository.db.Prepare("INSERT INTO Users (Name, EMail, Password, RegDate) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return 0, err
+	}
+	defer statement.Close()
+
+	regDate, err := time.Parse("02/01/2006", user.RegDate)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result, err := statement.Exec(user.Name, user.EMail, user.Pass, regDate)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println(result.RowsAffected())
+	return 1, nil
+
+	// lastID, err := result.LastInsertId()
+	// if err != nil {
+	// 	return 0, err
+	// }
+
+	// return uint64(lastID), nil
 }
