8dbbccb9fe3764869342d5482746b87511b59063
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-06-13 11:30:11 -0300

    E-Mail validation + Password hash

--- go.mod
+++ go.mod
@@ -5,12 +5,13 @@ go 1.20
 require github.com/gorilla/mux v1.8.0
 
 require (
+	github.com/badoux/checkmail v1.2.1
 	github.com/denisenkom/go-mssqldb v0.12.3
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.10.0
 )
 
 require (
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
 )
--- src/models/User.go
+++ src/models/User.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"api/src/security"
 	"errors"
 	"strings"
+
+	"github.com/badoux/checkmail"
 )
 
 // User model
@@ -15,32 +18,55 @@ type User struct {
 }
 
 func (user *User) CheckUser(level string) error {
+	//
 	if err := user.validate(level); err != nil {
 		return err
 	}
-	user.trim()
+
+	if err := user.trim(level); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (user *User) validate(level string) error {
+	//
 	if user.Name == "" {
 		return errors.New("nome é obrigatório e deve ser informado")
 	}
+
 	if user.EMail == "" {
 		return errors.New("E-Mail é obrigatório e deve ser informado")
 	}
+	if err := checkmail.ValidateFormat(user.EMail); err != nil {
+		return errors.New("E-Mail inválido")
+	}
+
 	if level == "ins" && user.Pass == "" {
 		return errors.New("senha é obrigatória e deve ser informada")
 	}
+
 	if user.RegDate == "" {
 		return errors.New("data de cadastro é obrigatória e deve ser informada")
 	}
+
 	return nil
 }
 
-func (user *User) trim() {
+func (user *User) trim(level string) error {
 	user.Name = strings.TrimSpace(user.Name)
 	user.EMail = strings.TrimSpace(user.EMail)
 	user.Pass = strings.TrimSpace(user.Pass)
 	user.RegDate = strings.TrimSpace(user.RegDate)
+
+	if level == "ins" {
+		hash, err := security.Hash(user.Pass)
+		if err != nil {
+			return err
+		}
+		user.Pass = string(hash)
+	}
+
+	return nil
 }
